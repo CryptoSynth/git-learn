316e7bcc7ed009ad391e3acba009efc16d129475
Leonardo J. Ramirez <lramirez.army@gmail.com>
2020-05-08 13:55:39 -0400

    github-demo-2 pushed changes to index.vue and added index2.vue

--- index.vue
+++ index.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="text-">
-    <h1>User 1</h1>
+    <h1>User 2 is now in the house, get rekt</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      items: ["Item 1", "Item 1", "Item 1", "Item 1"]
+    };
+  }
+};
 </script>
 
 <style>
